472c5710dc3622ae49175f3f43453eb69445e233
ZeroTheorem <zeratuliche@gmail.com>
2023-04-26 12:59:29 +0300

    ...

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import telebot
+from telebot import TeleBot
 import wikipedia
 
-bot = telebot.TeleBot("5672162831:AAGRpAFTkr0JPCD8kopSx702bsZNNf-sBb8")
+
+bot = TeleBot("5672162831:AAGRpAFTkr0JPCD8kopSx702bsZNNf-sBb8")
 wikipedia.set_lang("ru")
 
 
 @bot.message_handler(content_types=["text"])
-def get_response_from_wikipedia(message):
-    reqst = message.text
-    response = wikipedia.summary(reqst)
-    bot.send_message(message.chat.id, response)
+def get(message):
+    try:
+        resp = wikipedia.summary(message.text)
+        bot.send_message(message.chat.id, resp)
+    except wikipedia.DisambiguationError:
+        bot.send_message(message.chat.id, "Попробуйте более конкретный запрос")
+    except wikipedia.PageError:
+        bot.send_message(
+            message.chat.id, f"Старница с названием '{message.text} не найдена'")
 
 
-bot.polling(none_stop=True)
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
